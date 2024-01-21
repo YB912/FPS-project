67ffa8d9b17d9c751e92961cd7742cfb551611b8
arghavan <arghavan.habibibibalani@gmail.com>
2024-01-21 19:51:23 +0330

    UI controller script.

---
+++ Assets/Scripts/UI_Controller.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UI_Controller : MonoBehaviour
+{
+    public GameObject _settingPanel, _exitButton, _audioSlider;
+    public AudioSource _audioSource;
+    private bool _isPlaying = false;
+
+
+    void Start()
+    {
+        _audioSource = this.GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        _audioSource.volume = _audioSlider.GetComponent<Slider>().value;
+    }
+
+    public void StartButton()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void SettingButton()
+    {
+        _settingPanel.SetActive(true);
+        _exitButton.SetActive(true);
+    }
+    public void AboutUsButton()
+    {
+
+    }
+    public void ExitButton()
+    {
+        _exitButton.SetActive(false);
+        _settingPanel.SetActive(false);
+    }
+    //public void ExitButton2()
+    //{
+    //    _exitButton.SetActive(false);
+    //    _settingPanel.SetActive(false);
+    //}
+
+    public void AudioButton()
+    {
+        if( _isPlaying == false )
+        {
+            _audioSource.Play();
+            _isPlaying = true;
+        }
+        else if( _isPlaying == true )
+        {
+            _audioSource.Stop();
+            _isPlaying = false;
+        }
+
+    }
+   
+}
